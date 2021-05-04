64550302d707fc58187e99135d51a9c097ef6f06
Jean-Sébastien Goupil <contact@jsgoupil.com>
2021-05-04 14:31:28 -0700

    Initial commit.

---
+++ Extensions/BCGgs1128Extensions.cs
@@ -0,0 +1,19 @@
+﻿using BarcodeBakery.Common.GS1;
+
+namespace BarcodeBakery.Barcode
+{
+    /// <summary>
+    /// Extensions for the barcode GS1-128.
+    /// </summary>
+    public static class BCGgs1128Extensions
+    {
+        /// <summary>
+        /// Installs the default well known Application Identifiers from GS1.
+        /// </summary>
+        /// <param name="code">The GS1-128 instance.</param>
+        public static void InstallDefaultApplicationIdentifiers(this BCGgs1128 code)
+        {
+            code.SetApplicationIdentifiers(GS1AI.GetDefaultAIData());
+        }
+    }
+}
